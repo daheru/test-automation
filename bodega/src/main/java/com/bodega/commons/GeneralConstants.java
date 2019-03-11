794e70a6d00d70affef13da6c960af0decfd73aa
Daniel Hernandez Rubio  (Vendor) <vn0swlc@MX.wal-mart.com>
2019-03-11 13:52:27 -0600

    Business Classes are added

---
+++ bodega/src/main/java/com/bodega/commons/GeneralConstants.java
@@ -0,0 +1,16 @@
+package com.bodega.commons;
+
+
+public final class GeneralConstants {
+	
+	private GeneralConstants() {
+		
+	}
+	
+	public static final int SCROLL_DOWN = 0;
+	public static final int SCROLL_UP = 1;
+	public static final String ANDROID = "Android";
+	public static final String IOS = "IOS";
+	public static final String ID = "id";
+	public static final String XPATH = "xpath";
+}
---
+++ bodega/src/main/java/com/bodega/commons/MobileNamesConstants.java
@@ -0,0 +1,102 @@
+package com.bodega.commons;
+
+import com.bodega.util.PropertiesValuesUtil;
+
+public final class MobileNamesConstants {
+	
+	private MobileNamesConstants() {
+		
+	}
+	
+	public static final String NAV_BAR = PropertiesValuesUtil.getPropertyName("NAV_BAR");
+	public static final String NAV_BAR_PROFILE = PropertiesValuesUtil.getPropertyName("NAV_BAR_PROFILE");
+	public static final String NAV_BAR_DEPARTMENTS = PropertiesValuesUtil.getPropertyName("NAV_BAR_DEPARTMENTS");
+	public static final String NAV_BAR_HOME = PropertiesValuesUtil.getPropertyName("NAV_BAR_HOME");
+	
+	public static final String MENU_LIST = PropertiesValuesUtil.getPropertyName("MENU_LIST");
+	public static final String MENU_NAME = PropertiesValuesUtil.getPropertyName("MENU_NAME");
+	
+	public static final String LOGIN_BUTTON = PropertiesValuesUtil.getPropertyName("LOGIN_BUTTON");
+	public static final String LOGIN_EMAIL = PropertiesValuesUtil.getPropertyName("LOGIN_EMAIL");
+	public static final String LOGIN_PASS = PropertiesValuesUtil.getPropertyName("LOGIN_PASS");
+	public static final String WARNING_MESSAGE = PropertiesValuesUtil.getPropertyName("WARNING_MESSAGE");
+	public static final String NAME_FIELD = PropertiesValuesUtil.getPropertyName("NAME_FIELD");
+	public static final String PASS_FIELD = PropertiesValuesUtil.getPropertyName("PASS_FIELD");
+	public static final String ERROR_TEXT_FIELD = PropertiesValuesUtil.getPropertyName("ERROR_TEXT_FIELD");
+	public static final String FORGOT_PASS = PropertiesValuesUtil.getPropertyName("FORGOT_PASS");
+	public static final String REGISTER_BUTTON = PropertiesValuesUtil.getPropertyName("REGISTER_BUTTON");
+	
+	public static final String USER_EMAIL = PropertiesValuesUtil.getPropertyName("USER_EMAIL");
+	
+	public static final String BACK_BUTTON = PropertiesValuesUtil.getPropertyName("BACK_BUTTON");
+	public static final String LIST_TERMS = PropertiesValuesUtil.getPropertyName("LIST_TERMS");
+	public static final String LIST_TERM_ELEMENT = PropertiesValuesUtil.getPropertyName("LIST_TERM_ELEMENT");
+	public static final String ANDROID_NATIVE_ALLOW = PropertiesValuesUtil.getPropertyName("ANDROID_NATIVE_ALLOW");
+	public static final String ANDROID_NATIVE_DENY = PropertiesValuesUtil.getPropertyName("ANDROID_NATIVE_DENY");
+	public static final String ANDROID_DIALOG_CONT = PropertiesValuesUtil.getPropertyName("ANDROID_DIALOG_CONT");
+	public static final String SHOW_PASS = PropertiesValuesUtil.getPropertyName("SHOW_PASS");
+	
+	public static final String ACCOUNT_NAME_TEXTBOX = PropertiesValuesUtil.getPropertyName("ACCOUNT_NAME_TEXTBOX");
+	public static final String ACCOUNT_LASTNAME_TEXTBOX = PropertiesValuesUtil.getPropertyName("ACCOUNT_LASTNAME_TEXTBOX");
+	public static final String ACCOUNT_EMAIL_TEXTBOX = PropertiesValuesUtil.getPropertyName("ACCOUNT_EMAIL_TEXTBOX");
+	public static final String ACCOUNT_PASS_TEXTBOX = PropertiesValuesUtil.getPropertyName("ACCOUNT_PASS_TEXTBOX");
+	public static final String ACCOUNT_SHOW_PASS = PropertiesValuesUtil.getPropertyName("ACCOUNT_SHOW_PASS");
+	public static final String ACCOUNT_TERMS_LINK = PropertiesValuesUtil.getPropertyName("ACCOUNT_TERMS_LINK");
+	public static final String ACCOUNT_CREATE_BUTTON = PropertiesValuesUtil.getPropertyName("ACCOUNT_CREATE_BUTTON");
+	public static final String ACCOUNT_LOGIN_LINK = PropertiesValuesUtil.getPropertyName("ACCOUNT_LOGIN_LINK");
+	
+	public static final String HOME_SEARCH_BUTTON = PropertiesValuesUtil.getPropertyName("HOME_SEARCH_BUTTON");
+	public static final String HOME_SEARCH_FIELD = PropertiesValuesUtil.getPropertyName("HOME_SEARCH_FIELD");
+	public static final String HOME_SCAN = PropertiesValuesUtil.getPropertyName("HOME_SCAN");
+	public static final String HOME_CAR = PropertiesValuesUtil.getPropertyName("HOME_CAR");
+	public static final String HOME_OFFERS_FILTER = PropertiesValuesUtil.getPropertyName("HOME_OFFERS_FILTER");
+	public static final String HOME_PRODUCT_LIST = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_LIST");
+	public static final String HOME_PRODUCT_ELEMENT = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_ELEMENT");
+	public static final String HOME_EMPTY_CAR = PropertiesValuesUtil.getPropertyName("HOME_EMPTY_CAR");
+	public static final String HOME_EMPTY_CAR_ICON = PropertiesValuesUtil.getPropertyName("HOME_EMPTY_CAR_ICON");
+	public static final String HOME_EMPTY_CAR_BUTTON = PropertiesValuesUtil.getPropertyName("HOME_EMPTY_CAR_BUTTON");
+	public static final String HOME_PRODUCT_CONTAINER = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_CONTAINER");
+	public static final String HOME_PRODUCT_PROMOTION = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_PROMOTION");
+	public static final String HOME_PRODUCT_ITEM = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_ITEM");
+	public static final String HOME_PRODUCT_IMAGE = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_IMAGE");
+	public static final String HOME_PRODUCT_DESCRIPTION = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_DESCRIPTION");
+	public static final String HOME_PRODUCT_PRICE = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_PRICE");
+	public static final String HOME_PRODUCT_OLD_PRICE = PropertiesValuesUtil.getPropertyName("HOME_PRODUCT_OLD_PRICE");
+	
+	public static final String PRODUCT_CONTAINER = PropertiesValuesUtil.getPropertyName("PRODUCT_CONTAINER");
+	public static final String PRODUCT_TITLE = PropertiesValuesUtil.getPropertyName("PRODUCT_TITLE");
+	public static final String PRODUCT_IMAGE = PropertiesValuesUtil.getPropertyName("PRODUCT_IMAGE");
+	public static final String PRODUCT_PRICE = PropertiesValuesUtil.getPropertyName("PRODUCT_PRICE");
+	public static final String PRODUCT_SELL_BY = PropertiesValuesUtil.getPropertyName("PRODUCT_SELL_BY");
+	public static final String PRODUCT_UPC = PropertiesValuesUtil.getPropertyName("PRODUCT_UPC");
+	public static final String PRODUCT_ADD = PropertiesValuesUtil.getPropertyName("PRODUCT_ADD");
+	public static final String PRODUCT_PROMO = PropertiesValuesUtil.getPropertyName("PRODUCT_PROMO");
+	public static final String PRODUCT_DESC = PropertiesValuesUtil.getPropertyName("PRODUCT_DESC");
+	
+	public static final String SEARCH_RESULT_LIST = PropertiesValuesUtil.getPropertyName("SEARCH_RESULT_LIST");
+	public static final String SEARCH_RESULT_ELEMENT = PropertiesValuesUtil.getPropertyName("SEARCH_RESULT_ELEMENT");
+	public static final String SEARCH_WITHOUT_RESULT = PropertiesValuesUtil.getPropertyName("SEARCH_WITHOUT_RESULT");
+	public static final String SEARCH_WITHOUT_RESULT_HEADER = PropertiesValuesUtil.getPropertyName("SEARCH_WITHOUT_RESULT_HEADER");
+	public static final String SEARCH_WITHOUT_RESULT_ICON = PropertiesValuesUtil.getPropertyName("SEARCH_WITHOUT_RESULT_ICON");
+	public static final String SEARCH_WITHOUT_RESULT_BOTTOM = PropertiesValuesUtil.getPropertyName("SEARCH_WITHOUT_RESULT_BOTTOM");
+	
+	public static final String SCAN_CONTAINER = PropertiesValuesUtil.getPropertyName("SCAN_CONTAINER");
+	public static final String SCAN_TITLE = PropertiesValuesUtil.getPropertyName("SCAN_TITLE");
+	public static final String SCAN_MESSAGE = PropertiesValuesUtil.getPropertyName("SCAN_MESSAGE");
+	public static final String SCAN_BOX = PropertiesValuesUtil.getPropertyName("SCAN_BOX");
+	
+	public static final String CAR_CONTAINER = PropertiesValuesUtil.getPropertyName("CAR_CONTAINER");
+	public static final String CAR_ITEM = PropertiesValuesUtil.getPropertyName("CAR_ITEM");
+	public static final String CAR_BUY_BUTTON = PropertiesValuesUtil.getPropertyName("CAR_BUY_BUTTON");
+	public static final String CAR_DELETE_ITEM = PropertiesValuesUtil.getPropertyName("CAR_DELETE_ITEM");
+	public static final String CAR_DELETE_DIALOG = PropertiesValuesUtil.getPropertyName("CAR_DELETE_DIALOG");
+	public static final String CAR_CANCEL_BUTTON = PropertiesValuesUtil.getPropertyName("CAR_CANCEL_BUTTON");
+	public static final String CAR_CONFIRM_BUTTON = PropertiesValuesUtil.getPropertyName("CAR_CONFIRM_BUTTON");
+	
+	public static final String HELP_TITLE = PropertiesValuesUtil.getPropertyName("HELP_TITLE");
+	public static final String HELP_ICON = PropertiesValuesUtil.getPropertyName("HELP_ICON");
+	public static final String HELP_PHONE = PropertiesValuesUtil.getPropertyName("HELP_PHONE");
+	public static final String HELP_CALL_BUTTON = PropertiesValuesUtil.getPropertyName("HELP_CALL_BUTTON");
+	public static final String HELP_EMAIL_BUTTON = PropertiesValuesUtil.getPropertyName("HELP_EMAIL_BUTTON");
+	
+}
